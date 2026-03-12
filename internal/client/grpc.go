0076634421a2bed34ba67ebdb69c050a58d5f675
unsubstantiated-Script <jusfulton@gmail.com>
2026-03-12 09:58:07 -0500

    gRPC work

--- internal/client/grpc.go
+++ internal/client/grpc.go
@@ -1,1 +1,43 @@
 package client
+
+import (
+	"context"
+	pb "goDistributedSystem/pkg/pb"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GRPCClient struct {
+	conn   *grpc.ClientConn
+	client pb.NodeServiceClient
+}
+
+func NewGRPCClient(addr string) (*GRPCClient, error) {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	return &GRPCClient{
+		conn:   conn,
+		client: pb.NewNodeServiceClient(conn),
+	}, nil
+}
+
+func (c *GRPCClient) Close() {
+	c.conn.Close()
+}
+
+func (c *GRPCClient) ReportStatus(ctx context.Context, name string) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	resp, err := c.client.ReportStatus(ctx, &pb.Request{Data: name})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Data, nil
+}
